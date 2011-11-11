04c5358a2e77ae1a22b6d5bbd3574bc971568386
Ben Sanabria Carr <carrbs@cs.pdx.edu>
2011-11-11 14:58:24 -0800

    implementing the interface defined in TypeVI, getting close to compile

--- proj4/symbol/SymbolVisitor.java
+++ proj4/symbol/SymbolVisitor.java
@@ -17,7 +17,12 @@ public class SymbolVisitor implements TypeVI
     private MethodRec currMethod;   // the current method scope
     private boolean hasMain;        // whether "main" method is defined
 
-    public SymbolVisitor() { ... }
+    public SymbolVisitor()
+    {
+        symTable = new Table();
+        currClass = null;
+        currMethod = null;
+    }
 
     public void visit(Program n) throws Exception
     {
@@ -27,40 +32,46 @@ public class SymbolVisitor implements TypeVI
         setupClassHierarchy(n.cl);  // establish class hierarchy
     }
 
-    public void visit(ClassDecl n) throws Exception
-    {
-        // add a ClassRec to symTable 
-        // recursively process vl list and ml list
-    }
 
-    public void visit(MethodDecl n) throws Exception
-    {
-        // add a MethodRec to the current ClassRec
-        // recursively process fl list and vl list
-        // if the method is ’main’, check for violations
-        //  of main method’s rules
-    }
 
     public void visit(VarDecl n) throws Exception
     {
         // decide whether the var is a local var or a class var
         // (hint: use env variables currClass and currMethod)
         // add a VarRec to the proper ClassRec of MethodRec
+        if (currMethod)
+            currMethod.addLocal(n.var, n.t);
+        else
+            currClass.addClassVar(n.var, n.t, n.e);
     }
 
     public void visit(Formal n) throws Exception
     {
         // add a VarRec to the current MethodRec’s param list
+
     }
 
     //
     // LISTS --- use default traversal
     //
-    public void visit(List n) throws Exception {}
+    public void visit(AstList n) throws Exception {}
+
     public void visit(ClassDeclList n) throws Exception
     {
         for (int i = 0; i < n.size(); i++)
             n.elementAt(i).accept(this);
+    }
+
+    public void visit(ClassDecl n) throws Exception
+    {
+        symTable.addClass(n.cid); // add a ClassRec to symTable 
+        currClass = symTable.getClass(n.cid);
+        // recursively process vl list and ml list
+        for (int i = 0; i < n.vl.size(); i++)
+            n.vl.elementAt(i).accept(this);
+        for (int i = 0; i < n.ml.size(); i++)
+            n.ml.elementAt(i).accept(this);
+        currClass = null;
     }
 
     public void visit(VarDeclList n) throws Exception
@@ -69,6 +80,28 @@ public class SymbolVisitor implements TypeVI
             in.elementAt(i).accept(this);
     }
 
+    public void visit(MethodDecl n) throws Exception
+    {
+        // add a MethodRec to the current ClassRec
+        currClass.addMethod(n.mid, n.t);
+        currMethod = currClass.getMethod(n.mid);
+        // recursively process fl list and vl list
+        for (int i = 0; i < n.fl.size(); i++)
+            n.fl.elementAt(i).accept(this);
+        for (int i = 0; i < n.vl.size(); i++)
+            n.vl.elementAt(i).accept(this);
+        // if the method is ’main’, check for violations
+        //  of main method’s rules
+        if (n.id.s == "main")
+        {
+            if(!hasMain)
+                hasMain = true;
+            else 
+            throw new SymbolException("Every program must have one" +
+                " (and only one) main method.");
+        }
+        currMethod = null;
+    }
     //
     // TYPES --- return the nodes themselves
     //
