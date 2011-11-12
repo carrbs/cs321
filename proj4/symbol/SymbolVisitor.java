5bbfa69101243fd8c29403f515f0af654553b487
Ben Sanabria Carr <carrbs@cs.pdx.edu>
2011-11-11 16:31:06 -0800

    finished proj4

--- proj4/symbol/SymbolVisitor.java
+++ proj4/symbol/SymbolVisitor.java
@@ -24,6 +24,30 @@ public class SymbolVisitor implements TypeVI
         currMethod = null;
     }
 
+    private void setupClassHierarchy(ClassDeclList cl) throws Exception 
+    {
+        Vector<ClassDecl> work = new Vector<ClassDecl>();
+        Vector<String> done = new Vector<String>();
+        for (int i=0; i<cl.size(); i++)
+            work.add(cl.elementAt(i));
+        while (work.size() > 0)
+        {
+            for (int i=0; i<work.size(); i++)
+            {
+                ClassDecl cd = (ClassDecl) work.elementAt(i);
+                if (cd.pid != null) 
+                {
+                    if (!done.contains(cd.pid.s)) continue;
+                    ClassRec cr = symTable.getClass(cd.cid);
+                    ClassRec pr = symTable.getClass(cd.pid);
+                    cr.linkParent(pr);
+                }
+                done.add(cd.cid.s);
+                work.remove(cd);
+            }
+        }
+    }
+
     public void visit(Program n) throws Exception
     {
         n.cl.accept(this);
@@ -32,29 +56,43 @@ public class SymbolVisitor implements TypeVI
         setupClassHierarchy(n.cl);  // establish class hierarchy
     }
 
-
+    //
+    // LISTS --- use default traversal
+    //
+    public void visit(VarDeclList n) throws Exception
+    {
+        for (int i = 0; i < n.size(); i++)
+            n.elementAt(i).accept(this);
+    }
 
     public void visit(VarDecl n) throws Exception
     {
         // decide whether the var is a local var or a class var
         // (hint: use env variables currClass and currMethod)
         // add a VarRec to the proper ClassRec of MethodRec
-        if (currMethod)
+        if (currMethod != null)
+        {
+            if (currMethod.getParam(n.var) != null)
+                throw new SymbolException(n.var.s +" already defined as a param");
             currMethod.addLocal(n.var, n.t);
+        }
         else
             currClass.addClassVar(n.var, n.t, n.e);
     }
 
-    public void visit(Formal n) throws Exception
+    public void visit(FormalList n) throws Exception
     {
-        // add a VarRec to the current MethodRec’s param list
-
+        for (int i = 0; i < n.size(); i++)
+            n.elementAt(i).accept(this);
     }
 
-    //
-    // LISTS --- use default traversal
-    //
-    public void visit(AstList n) throws Exception {}
+   public void visit(Formal n) throws Exception
+    {
+        // add a VarRec to the current MethodRec’s param list
+        currMethod.addParam(n.id, n.t);
+    }
+
+   public void visit(AstList n) throws Exception {}
 
     public void visit(ClassDeclList n) throws Exception
     {
@@ -74,12 +112,12 @@ public class SymbolVisitor implements TypeVI
         currClass = null;
     }
 
-    public void visit(VarDeclList n) throws Exception
+
+    public void visit(MethodDeclList n) throws Exception
     {
         for (int i = 0; i < n.size(); i++)
-            in.elementAt(i).accept(this);
+            n.elementAt(i).accept(this);
     }
-
     public void visit(MethodDecl n) throws Exception
     {
         // add a MethodRec to the current ClassRec
@@ -92,31 +130,61 @@ public class SymbolVisitor implements TypeVI
             n.vl.elementAt(i).accept(this);
         // if the method is ’main’, check for violations
         //  of main method’s rules
-        if (n.id.s == "main")
+        if (n.mid.s.equals("main"))
         {
+            if (currClass.varCnt() > 0)
+                throw new SymbolException("main class are not allowed" +
+                    " to have data fields");
             if(!hasMain)
                 hasMain = true;
-            else 
-            throw new SymbolException("Every program must have one" +
-                " (and only one) main method.");
+            else
+                throw new SymbolException("Every program must have one" +
+                    " (and only one) main method.");
         }
         currMethod = null;
     }
+
+
     //
     // TYPES --- return the nodes themselves
     //
-    public Type visit(IntType n) { return n; }
-    public Type visit(BoolType n) { return n; }
+    public Type visit(BasicType n) { return n; }
+    public Type visit(ArrayType n) { return n; }
     public Type visit(ObjType n) throws Exception { return n; }
 
     //
-    // STMTS and EXPRS
+    // STATEMENTS
     //
     public void visit(StmtList n) throws Exception {}
     public void visit(Block n) throws Exception {}
     public void visit(Assign n) throws Exception {}
+    public void visit(CallStmt n) throws Exception {}
+    public void visit(If n) throws Exception {}
+    public void visit(While n) throws Exception {}
+    public void visit(Print n) throws Exception {}
+    public void visit(Return n) throws Exception {}
 
+    //
+    // Expressions
+    //
     public void visit(ExpList n) throws Exception {}
-    public Type visit(Binop n) throws Exception { return null; }
-    public Type visit(Relop n) throws Exception { return null; }
-}
+    public Type visit(Binop n) throws Exception {return null;}
+    public Type visit(Relop n) throws Exception {return null;}
+    public Type visit(Unop n) throws Exception {return null;}
+    public Type visit(ArrayElm n) throws Exception {return null;}
+    public Type visit(ArrayLen n) throws Exception {return null;}
+    public Type visit(Field n) throws Exception {return null;}
+    public Type visit(Call n) throws Exception {return null;}
+    public Type visit(NewArray n) throws Exception {return null;}
+    public Type visit(NewObj n) throws Exception {return null;}
+    public Type visit(Id n) throws Exception {return null;}
+    public Type visit(This n) throws Exception {return null;}
+
+    //
+    // Base values
+    //
+    public Type visit(IntVal n) {return null;}
+    public Type visit(FloatVal n) {return null;}
+    public Type visit(BoolVal n) {return null;}
+    public Type visit(StrVal n) {return null;}
+    }
